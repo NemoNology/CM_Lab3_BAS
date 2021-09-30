7a9c17484807fd1d07306165f042d76b4401190d
NemoNology <chicken.gang@mail.ru>
2021-09-30 19:53:08 +0900

    V6.1: небольшие изменения + надо переделать 1-ый шаг

--- CM_Lab3_BAS_V1.cpp
+++ CM_Lab3_BAS_V1.cpp
@@ -1,77 +1,91 @@
 ﻿#include <iostream>
+#include <iomanip>
 
 using namespace std;
 
 
+// Константы размрности массива
+const unsigned n = 5, m = 6;
+
+
 // Функция для вывода матрицы
-void Show_matr(float** p, unsigned &n, unsigned &m) {
+void Show_matr(double** p) {
     cout << "\n\n";
     for (unsigned i = 0; i < n; i++) {
-        cout << "\t" <<"|";
+        cout << "\t" << "|";
         for (unsigned j = 0; j < m; j++) {
-            cout << p[i][j] << "\t" <<"|";
+            cout << setw(12) << p[i][j] << "|";
         }
         cout << endl;
     }
     cout << "\n\n";
 }
 
-// 1-ый шаг метода Жаргана-Гаусса
-void step1(float** p) {
 
-    const unsigned n = 5;
-    float* fh[n];
+// 1-ый шаг метода Жаргана-Гаусса 
+void step1(double** p, unsigned k) {
 
-    for (unsigned i = 0; i < n; i++) {
-        if (p[i][i] == 0) {
-            for (unsigned j = i + 1; j < n; j++) {
-                if (p[j][i] != 0) {
-                    fh[i] = p[i];
-                    p[i] = p[j];
-                    p[j] = fh[i];
-                    break;
-                }
+    double* fh[n];
+
+    for (unsigned i = k; i < n; i++) {
+        for (unsigned j = i + 1; j < m; j++) {
+            if (p[i][i] == 0) {
+
+
             }
         }
-        
     }
 }
 
-// Функция для нахождения множителя строки для 2-ого шага
-void Find_mn(float* s1, float* s2, float &mn) {
-    
-
-}
-
 // 2-ой шаг метода Жаргана-Гаусса
-void step2(float** p, unsigned n, unsigned m) {
+void step2(double** p, unsigned k) {
 
-    float np_mn;
-    float* mn = &np_mn;
+    float z; // Пока тест...
+
     for (unsigned i = 0; i < n - 1; i++) {
-        for (unsigned j = 0; j < m - 1; j++) {
-            if (p[i][i] != 1) {
-
+        for (unsigned j = 0; j < m; j++) {
+            if ((p[i][i] != 1) & (i != k) & (j > k)) {
+                z = (p[i][j] * p[k][k] - p[i][k] * p[k][j]) / (p[k][k]);
+                p[i][j] = z;
             }
         }
+    }
+}
+
+// 3-ий шаг метода Жаргана-Гаусса
+void step3(double** p, unsigned k) {
+    for (unsigned i = k + 1; i < n; i++) {
+        p[i][k] *= 0;
+    }
+}
+
+// 4-ый шаг метода Жаргана-Гаусса
+void step4(double** p, unsigned k) {
+    for (unsigned j = 0; j < k; j++) {
+        p[k][m - j] /= p[k][k];
     }
 }
 
 
 // Метод Жордана-Гаусса в виде модуля, состоящего из множества функций (Шагов)
-void Method(float** aop, unsigned n, unsigned m) {
+void Method(double** aop, unsigned n, unsigned m) {
+
     // Вывод изначальной матрицы
     cout << "Изначальная матрица:";
-    Show_matr(aop, n, m);
+    Show_matr(aop);
 
     // Сообственно сам метод:
-    step1(aop);
-    step2(aop, n, m);
+    for (unsigned k = 0; k < n; k++) {
+        step1(aop, k);
+        step2(aop, k);
+        //step3(aop, k);
+        //step4(aop, k);
+    }
 
     
     // Вывод итоговой матрицы
     cout << "Итоговая матрица:";
-    Show_matr(aop, n, m);
+    Show_matr(aop);
 }
 
 
@@ -84,7 +98,7 @@ int main()
     const unsigned n = 5, m = 6;
     const unsigned * pn = &n, * pm = &m;
 
-    float my_mat[n][m] = { {0.42, -0.72, 0.02, -0.28, 0.2, 0.66},
+    double my_mat[n][m] = { {0.42, -0.72, 0.02, -0.28, 0.2, 0.66},
     {-0.67, 0.17, -0.86, 0.88, -0.88, 0.53},
     {0.14, -0.11, 0.86, -0.61, 0.61, 0.32},
     {0.83, -0.97, -0.29, -0.66, 0.7, -0.88},
@@ -93,7 +107,7 @@ int main()
 // Тестовый массив
 // ##############################################################
 
-    float tmat[n][m] = { {0.42, -0.72, 0.02, -0.28, 0.2, 0.66},
+    double tmat[n][m] = { {0.42, -0.72, 0.02, -0.28, 0.2, 0.66},
     {-0.67, 0.17, -0.86, 0.88, -0.88, 0.53},
     {0.14, -0.11, 0.86, -0.61, 0.61, 0.32},
     {0.83, -0.97, -0.29, -0.66, 0.7, -0.88},
@@ -102,7 +116,7 @@ int main()
 // ##############################################################
 
     // Не забудь поменять потом на свой массив
-    float* aop[n];
+    double* aop[n];
     for (int i = 0; i < n; i++) {
         aop[i] = tmat[i];
     }
