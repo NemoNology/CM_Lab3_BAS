dad8b3a38600c13b65505ae17779a1188b31697f
NemoNology <91414886+NemoNology@users.noreply.github.com>
2021-10-09 01:10:35 +0900

    V10.1
    V10.1: Пытался вычислить x1...

--- CM_Lab3_BAS.cpp
+++ CM_Lab3_BAS.cpp
@@ -15,7 +15,7 @@ void Show_matr(double** p) {
     for (unsigned i = 0; i < n; i++) {
         cout << "\t" << "|";
         for (unsigned j = 0; j < m; j++) {
-            cout << setw(12) << p[i][j] << "|";
+            cout << setw(12) << setprecision(8) << p[i][j] << "|";
         }
         cout << endl;
     }
@@ -45,51 +45,83 @@ void step1(double** p, unsigned k) {
     }
 }
 
-// 2-ой шаг метода Жаргана-Гаусса - вычисление a[i][j], при i != k & j > k
-void step2(double** p, unsigned k) {
+
+// 3-ий & 2-ой шаги метода Жаргана-Гаусса - вычисление a[i][j], при i != k & j > k & "Обнуление" элементов под a[k][k] 
+void step2a3(double** p, unsigned k) {
 
     for (unsigned i = k + 1; i < n; i++) {
         for (unsigned j = k + 1; j < m; j++) {
-            if ((p[i][i] != 1) & (i != k)) {
-                p[i][j] = (p[i][j] * p[k][k] - p[i][k] * p[k][j]) / (p[k][k]);
-                //cout << "step2:\n" << "k: " << k + 1 << "\ti: " << i + 1 << "\tj: " << j + 1;
-                //Show_matr(p);
-            }
+
+            p[i][j] = (p[i][j] * p[k][k] - p[i][k] * p[k][j]) / p[k][k]; //3-ий шаг метода Жаргана-Гаусса - вычисление a[i][j], при i != k & j > k
+
         }
-        break;
     }
-}
 
-// 3-ий шаг метода Жаргана-Гаусса - обнуление
-// Протестировано...
-void step3(double** p, unsigned k) {
+    for (unsigned i = k; i < n; i++) { // 2-ой шаг метода Жаргана-Гаусса - "Обнуление" элементов в строке k
+        if (i != k) {
 
-    double x = p[k][k];
-    
-    for (unsigned i = 0; i < n; i++) {
-        p[i][k] = 0;
-        p[k][k] = x;
-        //cout << "step3:\n" << "k: " << k + 1 << "\ti: " << i + 1 << "\tj: " << k + 1;
-        //Show_matr(p);
+            p[i][k] = 0;
+
+        }
     }
+
+    //cout << "step2:\n" << "k: " << k + 1;
+    //sShow_matr(p);
 }
 
 // 4-ый шаг метода Жаргана-Гаусса - Вычисление a[k][j]
 void step4(double** p, unsigned k) {
 
-    p[k][m - 1] /= p[k][k];
-    p[k][k] = 1;
+    for (unsigned j = m - 1; j + 1 > k; j--) {
 
-    // unsigned newj = m - 1; // Т.к. цикл for не воспроизводится по убыванию, то используем while...
-    //for (unsigned j = m - 1; j >= m - (k + 1); j--) {
-        //p[k][m - 1] /= p[k][k];
-    //}
+        p[k][j] /= p[k][k];
+
+        
+
+    }
+
+
+    //cout << "step4:\n" << "k: " << k + 1;
+    //Show_matr(p);
+
+}
+
+
+// Тестовый 5-ый шаг
+void step___5(double** p, unsigned k, double* kp) {
+
+    //cout << "p[k][n] * kp[k] = " << p[k][n] * kp[k];
+
+    for (unsigned i = k; i > n - 1; i++) {
+
+        p[i][n] = (p[i][n] * p[k][k] - p[i][k] * p[k][n]) / kp[k];
+        //p[i][n] = (p[i][n] * p[k][k] - p[i][k] * p[k][n]) / p[k][k];
+
+    }
+    
+
+    cout << "stept5:\n" << "k: " << k + 1;
+    Show_matr(p);
+        
     
 }
 
 
+// 5-ый шаг - вычисление a[i][j], при i = k & j < k
+void step5(double** p, unsigned k) {
+    for (unsigned i = k; i < n-1; i++) {
+        for (unsigned j = k; j < m; j++) {
+
+            p[i][j] = (p[i][j] * p[k][k] - p[i][k] * p[k][j]) / p[k][k]; //3-ий шаг метода Жаргана-Гаусса - вычисление a[i][j], при i != k & j > k
+
+            //cout << "step2:\n" << "k: " << k + 1 << "\ti: " << i + 1 << "\tj: " << j + 1;
+            //Show_matr(p);
+        }
+    }
+}
+
 // Метод Жордана-Гаусса в виде модуля, состоящего из множества функций (Шагов)
-void Method(double** aop, unsigned n, unsigned m) {
+void Method(double** aop, unsigned n, unsigned m, double* aok) {
 
     // Вывод изначальной матрицы
     cout << "Изначальная матрица:";
@@ -98,11 +130,25 @@ void Method(double** aop, unsigned n, unsigned m) {
     // Сообственно сам метод:
     for (unsigned k = 0; k < n; k++) {
         step1(aop, k);
-        step2(aop, k);
-        step3(aop, k);
+        step2a3(aop, k);
         step4(aop, k);
+        //cout << setw(50) << "----------\n";
+        //cout << "step " << k + 1 << ":\n";
+        //Show_matr(aop);
     }
 
+    cout << "step5:\n";
+    Show_matr(aop);
+
+    cout << setw(50) << "-----------------TEST ZONE----------------\n";
+
+    for (unsigned k = 0; k < n-1; k++) {
+
+        step___5(aop, k, aok);
+
+    }
+
+    cout << setw(50) << "-----------------TEST ZONE----------------\n";
 
     // Вывод итоговой матрицы
     cout << "Итоговая матрица:";
@@ -117,7 +163,8 @@ int main()
     // Мой массив
     // ##############################################################
 
-    double my_mat[n][m] = { {0.42, -0.72, 0.02, -0.28, 0.2, 0.66},
+    double my_mat[n][m] = {
+    {0.42, -0.72, 0.02, -0.28, 0.2, 0.66},
     {-0.67, 0.17, -0.86, 0.88, -0.88, 0.53},
     {0.14, -0.11, 0.86, -0.61, 0.61, 0.32},
     {0.83, -0.97, -0.29, -0.66, 0.7, -0.88},
@@ -126,7 +173,8 @@ int main()
     // Тестовый массив
     // ##############################################################
 
-    double tmat[n][m] = { {0.42, -0.72, 0.02, -0.28, 0.2, 0.66},
+    double tmat[n][m] = {
+    {0.42, -0.72, 0.02, -0.28, 0.2, 0.66},
     {-0.67, 0.17, -0.86, 0.88, -0.88, 0.53},
     {0.14, -0.11, 0.86, -0.61, 0.61, 0.32},
     {0.83, -0.97, -0.29, -0.66, 0.7, -0.88},
@@ -134,15 +182,32 @@ int main()
 
     // ##############################################################
 
+
+
+
+
     // Не забудь поменять потом на свой массив
+
+    // ############
+    // ################################
+    // ###############
+    // #####
+
+
+    double aok[n];
+
+
+
+
     double* aop[n];
     for (int i = 0; i < n; i++) {
         aop[i] = tmat[i];
+        aok[i] = my_mat[i][i];
     }
 
 
     // Тест 
-    Method(aop, n, m);
+    Method(aop, n, m, aok);
 
 
     system("pause"); // Окончание программы
